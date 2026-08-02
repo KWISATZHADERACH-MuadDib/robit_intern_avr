cebf096a095cc274845de0a5a7472b90a816358e
yep.hee1@gmail.com <yep.hee1@gmail.com>
2026-08-02 20:40:06 +0900

    day2 task2 update

--- day2/task2/task2/task2/main.c
+++ day2/task2/task2/task2/main.c
@@ -6,6 +6,7 @@
 
 void adc_init(void)
 {
+	// 기준전압: AVCC (REFS0=1, REFS1=0)
 	ADMUX = (1 << REFS0);
 	// ADC 활성화(ADEN) + 분주비 128 (ADPS2,1,0 = 1,1,1)
 	// 16MHz / 128 = 125kHz (ADC 권장 범위 50~200kHz)
@@ -21,8 +22,26 @@ uint16_t adc_read(uint8_t channel)
 	return ADC; // ADCL, ADCH 합쳐진 10bit 값 (0~1023)
 }
 
+// ADC(0~1023) 값에 따라 LED가 순차적으로 켜짐
+void led_sequential(uint16_t adc_value)
+{
+	uint8_t count = adc_value / 128;   // 1024/128 = 8단계 (0~8)
+	if (count > 8) count = 8;
+
+	uint8_t leds = 0;
+	for (uint8_t i = 0; i < count; i++)
+	{
+		leds |= (1 << i);   // count개만큼 LSB부터 순서대로 켤 위치 표시
+	}
+
+	PORTA = (uint8_t)~leds;   // 액티브 로우: 0 = 켜짐, 1 = 꺼짐이므로 반전
+}
+
 int main(void)
 {
+	DDRA = 0xFF;      // PORTA 전체를 출력으로 설정 (LED 구동용)
+	PORTA = 0xFF;     // LED 다 끔
+
 	char buf[17];
 	lcd_init();
 	adc_init();
@@ -32,8 +51,11 @@ int main(void)
 	{
 		uint16_t adc_value = adc_read(0);
 		unsigned long mv = (unsigned long)adc_value * 500UL / 1023UL;
-		unsigned int volts = mv / 100;		//정수
-		unsigned int frac  = mv % 100;		//소숫점
+		unsigned int volts = mv / 100;
+		unsigned int frac  = mv % 100;
+
+		led_sequential(adc_value);   // ADC 값에 따라 LED 순차 점등
+
 		snprintf(buf, sizeof(buf), "%4u  %u.%02uV", adc_value, volts, frac);
 		lcd_set_cursor(0, 1);
 		lcd_print(buf);
